3a6c34e47e3413c5af9b8ad281a663ca8d128d57
FatimahAlObaidan <69505635+FatimahAlObaidan@users.noreply.github.com>
2020-08-11 14:59:00 +0300

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo apt-get updape
-sudo apt update -y && sudo apt install -y curl vim jq git make docker.io
+sudo apt update -y 
+sudo apt install -y curl vim jq git make docker.io
 sudo usermod -aG docker ubuntu
